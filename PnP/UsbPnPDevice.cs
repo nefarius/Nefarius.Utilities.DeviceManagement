65a26f62f8642762f32b5ea259c2ecf06acbe25f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-05-08 19:18:58 +0200

    Added UsbPnPDevice

---
+++ PnP/UsbPnPDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nefarius.Utilities.DeviceManagement.PnP
+{
+    public class UsbPnPDevice : PnPDevice
+    {
+        protected UsbPnPDevice(string instanceId, DeviceLocationFlags flags) : base(instanceId, flags)
+        {
+            var className = GetProperty<string>(DevicePropertyDevice.Class);
+
+            if (!Equals(className, "USB"))
+                throw new ArgumentException("This device is not a USB device.");
+        }
+    }
+}
