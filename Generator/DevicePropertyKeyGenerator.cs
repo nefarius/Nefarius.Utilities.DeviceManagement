febec1dc6e6f5837b64e8673c2cb9c18512da9b6
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-09-07 15:47:45 +0200

    Added source generator skeleton for #2

---
+++ Generator/DevicePropertyKeyGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace DeviceManagementPropertiesGenerator
+{
+    [Generator]
+    public class DevicePropertyKeyGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
