44334f6f01261875cc6efb548b42ae4ed439d760
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-09-12 11:27:49 +0200

    Added Value property to ConfigManagerException

--- Exceptions/ConfigManagerException.cs
+++ Exceptions/ConfigManagerException.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using Windows.Win32.Devices.DeviceAndDriverInstallation;
 
-namespace Nefarius.Utilities.DeviceManagement.Exceptions
-{
-    /// <summary>
-    ///     A Configuration Manager API has failed.
-    /// </summary>
-    public class ConfigManagerException : Exception
-    {
-        internal ConfigManagerException(string message) : base(message)
-        {
-        }
+namespace Nefarius.Utilities.DeviceManagement.Exceptions;
 
-        internal ConfigManagerException(string message, CONFIGRET result) : this(message)
-        {
-        }
+/// <summary>
+///     A Configuration Manager API has failed.
+/// </summary>
+public class ConfigManagerException : Exception
+{
+    internal ConfigManagerException(string message) : base(message)
+    {
     }
+
+    internal ConfigManagerException(string message, CONFIGRET result) : this(message)
+    {
+        Value = (uint)result;
+    }
+
+    /// <summary>
+    ///     The CONFIGRET value of the error.
+    /// </summary>
+    public uint Value { get; }
 }
--- Exceptions/Win32Exception.cs
+++ Exceptions/Win32Exception.cs
@@ -1,33 +1,32 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace Nefarius.Utilities.DeviceManagement.Exceptions
+namespace Nefarius.Utilities.DeviceManagement.Exceptions;
+
+/// <summary>
+///     A Win32 API has failed.
+/// </summary>
+public class Win32Exception : Exception
 {
     /// <summary>
     ///     A Win32 API has failed.
     /// </summary>
-    public class Win32Exception : Exception
+    /// <param name="message">The error message.</param>
+    internal Win32Exception(string message) : base(message)
     {
-        /// <summary>
-        ///     A Win32 API has failed.
-        /// </summary>
-        /// <param name="message">The error message.</param>
-        internal Win32Exception(string message) : base(message)
-        {
-        }
-
-        /// <summary>
-        ///     A Win32 API has failed.
-        /// </summary>
-        /// <param name="message">The error message.</param>
-        /// <param name="errorCode">The error code.</param>
-        internal Win32Exception(string message, int errorCode) : this(message)
-        {
-        }
-
-        /// <summary>
-        ///     The native Windows error code.
-        /// </summary>
-        public int ErrorCode { get; set; } = Marshal.GetLastWin32Error();
     }
+
+    /// <summary>
+    ///     A Win32 API has failed.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <param name="errorCode">The error code.</param>
+    internal Win32Exception(string message, int errorCode) : this(message)
+    {
+    }
+
+    /// <summary>
+    ///     The native Windows error code.
+    /// </summary>
+    public int ErrorCode { get; set; } = Marshal.GetLastWin32Error();
 }
