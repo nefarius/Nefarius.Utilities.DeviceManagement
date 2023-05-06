b4e5e1c8f6a5053140f72d0ed66c03117bd9dada
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-05-06 14:02:58 +0200

    Added MultiSzPointerToStringArray overload

--- src/PnP/DeviceClassFilters.cs
+++ src/PnP/DeviceClassFilters.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using Windows.Win32;
+using Windows.Win32.Foundation;
 using Windows.Win32.System.Registry;
 
 using Microsoft.Win32.SafeHandles;
@@ -13,13 +14,48 @@ public sealed class DeviceClassFilters
 {
     internal DeviceClassFilters() { }
 
-    public static void AddUpper(Guid classGuid, string service)
+    public static unsafe void AddUpper(Guid classGuid, string service)
     {
-        SafeRegistryHandle key = PInvoke.SetupDiOpenClassRegKey(classGuid, (uint)REG_SAM_FLAGS.KEY_ALL_ACCESS);
+        using SafeRegistryHandle key = PInvoke.SetupDiOpenClassRegKey(classGuid, (uint)REG_SAM_FLAGS.KEY_ALL_ACCESS);
 
         if (key.IsInvalid)
         {
             throw new Win32Exception("Failed to open class registry key");
+        }
+
+        const string filter = "LowerFilters";
+        REG_VALUE_TYPE type;
+        uint sizeRequired = 0, reserved = 0;
+
+        var status = PInvoke.RegQueryValueEx(
+            key,
+            filter,
+            ref reserved,
+            &type,
+            null,
+            &sizeRequired
+        );
+
+        // value exists
+        if (status == WIN32_ERROR.ERROR_SUCCESS)
+        {
+            var buffer = stackalloc byte[(int)sizeRequired];
+
+            status = PInvoke.RegQueryValueEx(
+                key,
+                filter,
+                ref reserved,
+                &type,
+                buffer,
+                &sizeRequired
+            );
+
+            if (status != WIN32_ERROR.ERROR_SUCCESS)
+            {
+                throw new Win32Exception("Failed to query value");
+            }
+            
+            
         }
     }
 
