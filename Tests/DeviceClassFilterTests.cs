fd94fcd7ea5d5c9e1cba866572bd249a3df687c3
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-05-06 21:55:55 +0200

    Update DeviceClassFilterTests.cs

--- Tests/DeviceClassFilterTests.cs
+++ Tests/DeviceClassFilterTests.cs
@@ -14,6 +14,10 @@ public class DeviceClassFilterTests
     ///     Requires HidHide to be installed to work!
     /// </summary>
     private const string Service01 = "HidHide";
+
+    /// <summary>
+    ///     This service must not exist!
+    /// </summary>
     private const string Service02 = "HidVibe";
 
     /// <summary>
@@ -38,7 +42,7 @@ public class DeviceClassFilterTests
         Assert.That(DeviceClassFilters.GetUpper(DeviceClassIds.XnaComposite)!.Count(), Is.EqualTo(1));
         CollectionAssert.Contains(DeviceClassFilters.GetUpper(DeviceClassIds.XnaComposite), Service01);
 
-        // add invalid service
+        // add invalid service, has to throw exception
         Assert.Throws(typeof(DriverServiceNotFoundException), () => DeviceClassFilters.AddUpper(DeviceClassIds.XnaComposite, Service02));
     }
 }
