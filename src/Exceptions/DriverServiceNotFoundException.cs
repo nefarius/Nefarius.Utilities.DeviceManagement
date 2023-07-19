100e8495f1623fdaddb2f653ade0bf41e2afd389
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-07-19 17:13:24 +0200

    Update DriverServiceNotFoundException.cs

--- src/Exceptions/DriverServiceNotFoundException.cs
+++ src/Exceptions/DriverServiceNotFoundException.cs
@@ -6,8 +6,10 @@ namespace Nefarius.Utilities.DeviceManagement.Exceptions;
 ///     A driver service wasn't found on the local machine.
 /// </summary>
 [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
 public class DriverServiceNotFoundException : Win32Exception
 {
+    /// <inheritdoc />
     internal DriverServiceNotFoundException(string message) : base(message)
     {
     }
