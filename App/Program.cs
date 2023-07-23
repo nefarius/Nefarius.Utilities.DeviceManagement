ef7a5af82efde297ed06083cef8a76afdc9e958b
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-07-23 18:26:40 +0200

    Added test code

--- App/Program.cs
+++ App/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Nefarius.Utilities.DeviceManagement.PnP;
+
+const string instanceId = @"USB\VID_054C&PID_0CE6&MI_03\9&DC32669&3&0003";
+        
+PnPDevice device = PnPDevice.GetDeviceByInstanceId(instanceId);
+        
+device.InstallCustomDriver("winusb.inf");
